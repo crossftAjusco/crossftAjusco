ee41c3e4d4729a4270e3c43dcf4045aadd537bee
mon0macal7 <monhdz07@gmail.com>
2022-05-17 19:29:47 -0500

    create testimony component

---
+++ src/LandingPage/navBar/Testimony.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import "./Testimony.css";
+{
+  /*import slider, slick,slick carousel,avatar */
+}
+const Testimony = () => {
+  return (
+    <div>
+      {/*
+      <div style={{display:"flex",
+    justifyContent:"center"}}>
+    <div style={{width:"50%"}}>
+        <Slider>
+           <Card img= ""/>
+           <Card img= ""/>
+           <Card img= ""/>
+        </Slider>
+      </div>
+      );
+      };
+ */}
+      {/*
+          const target = ({img})=>{
+          return(
+          <div className="">
+          <Avatar
+          imgProps={{style:{borderRadius:} }}
+           src={img} style= {{
+            width:120, 
+            height:120,
+            border:"1px solid lightgray",
+            padding:7,
+        }}/>
+          <p>texto</p>
+          <span>nombre de la persona <span>
+          </div>
+          )
+        }
+         */}
+      <h1 className="texto">testimons</h1>
+    </div>
+  );
+};
+
+export default Testimony;
