39af1212fc6e1151e44fcbc5b8cd123daa2930b2
mon0macal7 <monhdz07@gmail.com>
2022-05-20 19:01:18 -0500

    modify price style

--- src/LandingPage/navBar/Precios.jsx
+++ src/LandingPage/navBar/Precios.jsx
@@ -1,16 +1,30 @@
 import React from "react";
 import "./Precios.css";
-import mensualidad from "../../assets/lp_imgs/estandar.jpg";
-import enfocado from "../../assets/lp_imgs/enfocado.jpg";
-import nutri from "../../assets/lp_imgs/nutriconComp.jpg";
+import mensualidad from "../../assets/lp_imgs/newST.png";
+import enfocado from "../../assets/lp_imgs/focused.png";
+import nutri from "../../assets/lp_imgs/nutriN.png";
 
 const Precios = () => {
   return (
-    <div className="cost">
-      <img className="pricestyle" src={mensualidad} alt="month" />
-      <img src={enfocado} alt="visit" />
-      <img src={nutri} alt="focus" />
-    </div>
+    <>
+      <section className="sect">
+        <div>
+          <h1 className="sloggi">Entrenamientos</h1>
+        </div>
+        <div className="cost">
+          <div className="pricestyle">
+            <img className="sizeprice" src={mensualidad} alt="month" />
+          </div>
+          <div className="pricestyle">
+            {" "}
+            <img className="sizeprice" src={enfocado} alt="visit" />
+          </div>
+          <div className="pricestyle">
+            <img className="sizeprice" src={nutri} alt="focus" />
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
 
