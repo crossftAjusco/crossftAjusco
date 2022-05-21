1332756875ae06761610742e455a4a368ad9ceb7
mon0macal7 <monhdz07@gmail.com>
2022-05-21 16:01:04 -0500

    add testimony text

--- src/LandingPage/navBar/Testimony.jsx
+++ src/LandingPage/navBar/Testimony.jsx
@@ -29,11 +29,7 @@ const Testimony = () => {
   profesor logre romper mis limites, ahora voy por mi segundo maraton y esta vez voy
 por el primer lugar`;*/
   }
-  const vania = `Lalo es una persona muy preparada que sabe lo que hace al planear y
-        estructurar los entrenamientos, definitivamente una persona regresa al
-        tomar una clase de prueba. Es dinámico, nada rutinario y siempre hay
-        algo qué aprender para perfeccionar las técnicas. CrossFT es mi lugar
-        favorito para ponerme en forma!`;
+
   return (
     <section className="wall">
       <div
@@ -45,9 +41,32 @@ por el primer lugar`;*/
             Testimonios
           </h1>
           <Slider prevArrow={<PreviousBtn />} nextArrow={<NextBtn />} dots>
-            <Card img="https://i.ibb.co/g4mnQr1/Captura-de-pantalla-2022-05-18-a-la-s-14-10-17.png" />
-            <Card img="https://hagadone.media.clients.ellingtoncms.com/ARTICLE_190629924_AR_0_VKRMBLFPLLVN_t1170.jpg?5cc718665ab672dba93d511ab4c682bb370e5f86" />
-            <Card img="https://lionsheadcf.com/wp-content/uploads/2020/08/lindsay-davis-scaled.jpg" />
+            <Card
+              img="https://pbs.twimg.com/profile_images/1431129476151357441/AZmUEptx_400x400.jpg"
+              paragraph="Mi experiencia en el ejercicio lleva apenas 3 meses y medio, después de casi 20 años de inactividad física. Encontré en redes sociales a Cross FT Ajusco y llegó por medio de una dinámica, la cual fue 2 semanas de entrenamiento gratuitas, a partir de ahí ya no me fui, hoy puedo decir que estoy frente a un entrenador muy profesional (Lalo Romero) ya que sabe, entiende y adecua la forma de cada uno de los inscritos en su proyecto. En lo personal para mí ha Sido muy grato ya que en este poco tiempo he perdido casi 10 kilos, eso me sigue motivando día con día a qué nunca es tarde para empezar o regresar a hacer actividad física..."
+              author="Alejandro García"
+            />
+            <Card
+              img="https://i.ibb.co/g4mnQr1/Captura-de-pantalla-2022-05-18-a-la-s-14-10-17.png"
+              paragraph="Lalo es una persona muy preparada que sabe lo que hace al planear y estructurar los entrenamientos, definitivamente una persona regresa al tomar una clase de prueba. Es dinámico, nada rutinario y siempre hay algo qué aprender para perfeccionar las técnicas. CrossFT es mi lugar favorito para ponerme en forma! "
+              author="Vania Ramírez"
+            />
+            <Card
+              img="https://www.okchicas.com/wp-content/uploads/2020/06/%C3%87a%C4%9Fatay-Ulusoy-525x700.jpg"
+              paragraph="Entrenar en CrossFt Ajusco cambió mi realidad, mi cotidianidad. Ahora sé y entiendo que se puede vivir de otro modo. Entendí que mi cuerpo tiene otra forma y otro sentir cuando está en movimiento incluso cuando esta en reposo. Y no, no es vanidad, es estar sano y listo para el mañana. Perdí la flojera de levantarme temprano, perdí algunos dolores de espalda y las canas que me salían por estrés. Y creo que también perdí la pena de entrenar solo, pero siempre es mejor estar acompañado."
+              author="Unknown"
+            />
+            <Card
+              img="https://anabelavila.com/wp-content/uploads/2016/02/beneficios-crossfit-mujeres.webp"
+              paragraph="Iniciar con el entrenamiento en Cross F T Ajusco me ha hecho entender que mi cuerpo es maravilloso, que la disciplina es un punto importante en todos los ámbitos de nuestras vidas, todo inicio por salud, hoy puedo decir que vivo enamorada del entrenamiento y de la fuerza que puedo mostrarme a mi misma gracias a la motivación y acompañamiento de Eduardo y Edgar. Cross F T Ajusco es una analogía de vida, para conseguir resultados es necesario poner mucho empeño, dedicación, paciencia, amor, errores y posibles caídas, pero te levantas y continúas."
+              author="Marcela"
+            />
+            <Card
+              img="https://pbs.twimg.com/profile_images/1342152259359137792/BX1YBK2E_400x400.jpg"
+              paragraph="Iniciar con el entrenamiento en Cross F T Ajusco me ha hecho entender que mi cuerpo es maravilloso, que la disciplina es un punto importante en todos los ámbitos de nuestras vidas, todo inicio por salud, hoy puedo decir que vivo enamorada del entrenamiento y de la fuerza que puedo mostrarme a mi misma gracias a la motivación y acompañamiento de Eduardo y Edgar. 
+              Cross F T Ajusco es una analogía de vida, para conseguir resultados es necesario poner mucho empeño, dedicación, paciencia, amor, errores y posibles caídas, pero te levantas y continúas."
+              author="Frank"
+            />
           </Slider>
         </div>
       </div>
@@ -55,7 +74,7 @@ por el primer lugar`;*/
   );
 };
 
-const Card = ({ img }) => {
+const Card = ({ img, paragraph, author }) => {
   return (
     <div
       style={{
@@ -76,13 +95,7 @@ const Card = ({ img }) => {
           marginBottom: 15,
         }}
       />
-      <p style={{ color: "white", fontFamily: "monospace" }}>
-        Lalo es una persona muy preparada que sabe lo que hace al planear y
-        estructurar los entrenamientos, definitivamente una persona regresa al
-        tomar una clase de prueba. Es dinámico, nada rutinario y siempre hay
-        algo qué aprender para perfeccionar las técnicas. CrossFT es mi lugar
-        favorito para ponerme en forma!
-      </p>
+      <p className="pghp">{paragraph}</p>
       <p
         style={{
           fontWeight: 400,
@@ -91,7 +104,7 @@ const Card = ({ img }) => {
           marginTop: 25,
         }}
       >
-        <span>Vania Ramírez </span>, Frontend Developer Sr.
+        <span>{author}</span>
       </p>
     </div>
   );
