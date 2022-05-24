98b6f2e5f0db1a8d075540c4a802c27344027d6d
Jazo <jazoluxetveritas@gmail.com>
2022-05-24 13:57:09 -0500

    test edit modal

--- src/UserPage/userView/UserModal.jsx
+++ src/UserPage/userView/UserModal.jsx
@@ -8,34 +8,50 @@ const db = getFirestore(app);
 
 
 //creamos la función para traer los datos de la fila seleccionada en un modal
-const UserModal = ({show, setShow, modalUserData, tipo, data, id}) => {
+const UserModal = ({show, setShow, modalUserData, tipo, data, id, keys}) => {
     const handleClose = () => setShow(false);
     const [dataUser, setUserData] = useState({}); 
+    const [dataUser2, setUserData2] = useState(''); 
+
     console.log(tipo)
+    console.log(keys)
     //creamos la funcion para actualizar los datos del modal
     const updateA = async (e) => {
     e.preventDefault()
     console.log(modalUserData.phone)
     const docRef = doc (db, `Users/${id}`);
-    console.log(id)
-    
-    console.log(docRef)
-    //console.log(llave)
-    
 
+    console.log(id)
+    console.log(docRef)
     await updateDoc(docRef, {
        phone: dataUser,
-       
+
     });
-      setUserData({});
+      setUserData({});      
       handleClose();
     };
+
+    const updateB = async (e) => {
+      e.preventDefault()
+      console.log(modalUserData.allergies)
+      const docRef = doc (db, `Users/${id}`);
+  
+      console.log(id)
+      console.log(docRef)
+      await updateDoc(docRef, {
+         allergies: dataUser2
+      });
+        
+        setUserData2('');
+        handleClose();
+      };
+
 
     
     return(
         <>
-        <div>
-        <Modal show={show} onHide={handleClose} key={id}>
+        <div >
+        <Modal show={show} onHide={handleClose} key={keys='key-0'}>
       <Modal.Header>
         <Modal.Title>Cambiar Datos:</Modal.Title>
       </Modal.Header>
@@ -60,7 +76,35 @@ const UserModal = ({show, setShow, modalUserData, tipo, data, id}) => {
         </Button>
       </Modal.Footer>
     </Modal>
-    
+    </div>
+
+
+    <div>
+        <Modal show={show} onHide={handleClose} key={keys='key-1'}>
+      <Modal.Header>
+        <Modal.Title>Cambiar Datos:</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form >
+          <Form.Group className="mb-3" >
+            <Form.Label>{tipo}</Form.Label>
+            <Form.Control
+              placeholder={data}
+              autoFocus 
+              onChange={(e) => setUserData2 (e.target.value)}
+            />
+          </Form.Group>
+        </Form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={handleClose}>
+          Cancelar
+        </Button>
+        <Button variant="primary" onClick={updateB} type="submit" >
+          Guardar cambios
+        </Button>
+      </Modal.Footer>
+    </Modal>
     </div>
     </>
     
