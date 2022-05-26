902d8950714bc139b227c3b10c0a614bd82d6f04
mon0macal7 <monhdz07@gmail.com>
2022-05-25 19:00:13 -0500

    modify biopic component

--- src/LandingPage/navBar/Biopic.jsx
+++ src/LandingPage/navBar/Biopic.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import CloseIcon from "@mui/icons-material/Close";
 import "./Biopic.css";
 import diplom1 from "../../assets/lp_imgs/dip1V.jpeg";
 import diplom2 from "../../assets/lp_imgs/dip2H.jpeg";
@@ -11,40 +10,64 @@ import lalo from "../../assets/lp_imgs/laloR.png";
 const Biopic = () => {
   return (
     <div className="biopic">
-      <div className="boxZero" style={{ width: "200px", height: "300px" }}>
-        <section className="bioText">
-          <h3 className="encabezado">Eduardo Romero Flores</h3>
-          <img
-            className="picL"
-            src={lalo}
-            height="300"
-            width="220"
-            alt="Coach"
-          />
-          <p>
-            Desde pequeño adquirí dos pasiones que nunca he dejado: el
-            <b>deporte</b>y la lectura. En la escuela primaria tuve contacto con
-            el atletismo donde nació y aún pervive mi gusto por correr. Un
-            tiempo apasionado del <b>gimnasio</b>, desde siempre de los espacios
-            abiertos, el deporte siempre ha sido parte de mi vida. Escalar,
-            practicar alpinismo, senderismo, correr en montañas; el
-            <b>entrenamiento funcional</b>
-            así como un poco de artes marciales han marcado mi formación. Además
-            del gusto por el conocimiento; de vital importancia en la práctica
-            deportiva ya sea esta recreativa, amateur o profesional.
-            <br />
-            Cuento con 2 certificaciones oficiales:
+      <div style={{ width: "320px", height: "550px" }} className="bioText">
+        <h3 className="encabezado">Eduardo Romero Flores</h3>
+        <img className="picL" src={lalo} />
+        <p>
+          Desde pequeño adquirí dos pasiones que nunca he dejado: el{" "}
+          <b>deporte</b> y la lectura. En la escuela primaria tuve contacto con
+          el atletismo en donde nació y aún pervive mi gusto por correr. Después
+          de un tiempo me convertí en un apasionado del <b>gimnasio</b> además
+          de ejercitarme en espacios abiertos; me gusta escalar, practicar
+          alpinismo, senderismo y correr en las montañas. El
+          <b>entrenamiento funcional</b> y las artes marciales han marcado mi
+          formación; además del gusto por el conocimiento; de vital importancia
+          en la práctica deportiva ya sea esta recreativa, amateur o
+          profesional.
+          <br />
+          Cuento con 2 certificaciones oficiales:
+          <em>
             <li>
               Entrenador deportivo especializado en 'Entrenamiento Funcional'
             </li>
             <li>Entrenador en 'Acondicionamiento Físico'</li>
-          </p>
-        </section>
-        <img className="pictures" src={diplom3} alt="certificado" />
-        <img className="pictures" src={diplom1} alt="certificado" />
-        <img className="pictures" src={diplom2} alt="certificado" />
-        <img className="pictures" src={diplom4} alt="certificado" />
-        <img className="pictures" src={diplom5} alt="certificado" />
+          </em>
+        </p>
+      </div>
+      <div className="ImgCert">
+        <img
+          style={{ width: "320px", height: "550px" }}
+          src={diplom3}
+          alt="certificado"
+        />
+      </div>
+      <div className="ImgCert">
+        <img
+          style={{ width: "320px", height: "520px" }}
+          src={diplom1}
+          alt="certificado"
+        />
+      </div>
+      <div className="ImgCert">
+        <img
+          style={{ width: "320px", height: "520px" }}
+          src={diplom2}
+          alt="certificado"
+        />
+      </div>
+      <div className="ImgCert">
+        <img
+          style={{ width: "320px", height: "520px" }}
+          src={diplom4}
+          alt="certificado"
+        />
+      </div>
+      <div className="ImgCert">
+        <img
+          style={{ width: "320px", height: "570px" }}
+          src={diplom5}
+          alt="certificado"
+        />
       </div>
     </div>
   );
